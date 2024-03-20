bc9e38f4265952e2c33f223aca14b166b2ee55c2
cdecayeux <calvin.decayeux@caensup.fr>
2024-03-20 12:16:52 +0100

    Ajout validation models

--- src/main/kotlin/fr/backend/models/Restaurant.kt
+++ src/main/kotlin/fr/backend/models/Restaurant.kt
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.ManyToOne
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Pattern
 import java.util.*
 
 @Entity
@@ -17,11 +19,23 @@ open class Restaurant {
     @NotBlank
     open var decription: String? = null
     @Column(columnDefinition = "TEXT")
+    @NotBlank
     open var address1: String? = null
+    @NotBlank
     open var address2: String? = null
+    @NotBlank
     open var postalCode: String? = null
+    @NotBlank
     open var city: String? = null
-    open var coordGPS: String? = null
+
+    @Pattern(regexp = "[-+]?([1-8]?\\d(\\.\\d+)?|90(\\.0+)?)", message = "Latitude must be a valid GPS coordinate")
+    @NotEmpty
+    open var latitude: String? = null
+
+    @Pattern(regexp = "[-+]?((1[0-7]|[1-9])?\\d(\\.\\d+)?|180(\\.0+)?)", message = "Longitude must be a valid GPS coordinate")
+    @NotEmpty
+    open var longitude: String? = null
+
     open var approuve: Boolean = false
     open var idTheme: UUID = UUID.randomUUID()
 
