6515d24e4f85379a9b4bd6d5e0e46b3656ac415c
ltheaudin <loic.theaudin@gmail.com>
2024-03-20 12:16:02 +0100

    Ajout Validation models

--- src/main/kotlin/fr/backend/models/Plat.kt
+++ src/main/kotlin/fr/backend/models/Plat.kt
@@ -4,18 +4,28 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.ManyToOne
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 import java.util.*
 
 @Entity
 open class Plat {
     @Id
     open var id: UUID = UUID.randomUUID()
+    @NotBlank
     open var name: String? = null
+    @NotNull(message = "L'image ne peut pas être nulle")
     open var image: String? = null
     @Column(columnDefinition = "TEXT")
+    @NotBlank
     open var description: String? = null
+    @Positive
     open var price: Int? = null
+    @NotEmpty
     open var actif: Boolean? = false
+    @NotEmpty
     open var duJour: Boolean? = false
 
     @ManyToOne
--- src/main/kotlin/fr/backend/models/TypePlat.kt
+++ src/main/kotlin/fr/backend/models/TypePlat.kt
@@ -2,12 +2,14 @@ package fr.backend.models
 
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
+import jakarta.validation.constraints.NotBlank
 import java.util.*
 
 @Entity
 class TypePlat {
     @Id
     open var id: UUID = UUID.randomUUID()
+    @NotBlank
     open var libelle: String? = null
 
 }
