e58a737e7b99aed56b22dad3d3937284c24e0a11
Sacha Guignard <sacha.guignard@caensup.fr>
2024-03-20 09:57:39 +0100

    suppresion des repository qui on 2 id en manytoone

--- src/main/kotlin/fr/backend/repository/EvalOfficielleRepository.kt
+++
@@ -1,9 +0,0 @@
-package fr.backend.repository
-
-import fr.backend.models.EvalOfficielle
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
-import java.util.*
-
-@RepositoryRestResource(collectionResourceRel = "evalofficielle", path = "evalofficielle")
-interface EvalOfficielleRepository : JpaRepository<EvalOfficielle, UUID>
--- src/main/kotlin/fr/backend/repository/OffreRestoRepository.kt
+++
@@ -1,9 +0,0 @@
-package fr.backend.repository
-
-import fr.backend.models.OffreResto
-import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
-import java.util.*
-
-@RepositoryRestResource(collectionResourceRel = "offreresto", path = "offreresto")
-interface OffreRestoRepository : JpaRepository<OffreResto, UUID>
