fe69d606cae4c4ddb92a834a469943b629de84df
Sacha Guignard <sacha.guignard@caensup.fr>
2024-05-31 23:03:11 +0200

    ajout de correction cross

--- src/main/kotlin/fr/backend/repository/PlatRepository.kt
+++ src/main/kotlin/fr/backend/repository/PlatRepository.kt
@@ -6,6 +6,8 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.web.bind.annotation.CrossOrigin
 import java.util.*
 
-@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "plat", path = "plat")
-interface PlatRepository : JpaRepository<Plat, UUID>
+@CrossOrigin(origins = ["*"])
+interface PlatRepository : JpaRepository<Plat, UUID> {
+    fun findByRestaurantId(restaurantId: UUID): List<Plat>
+}
